7c425d5723a9e0e658a1c7ddeb2ea8f6a71379e7
kestasli <kestas.li@icloud.com>
2023-09-15 20:20:02 +0300

    added reset state

--- mg_timer.ino
+++ mg_timer.ino
@@ -9,9 +9,16 @@
 
 #define SCREEN_W 320
 #define SCREEN_H 172
-const int TIMER_PIN = 0;
-const int RESET_PIN = 14;
-const int COUNTER_DELAY = 3000000;
+
+
+#define TIMER_PIN 0
+#define RESET_PIN = 14
+#define COUNTER_DELAY 3000000
+
+#define TIMER_RUN 0
+#define TIMER_STOP 1
+#define TIMER_RESET 2
+
 //ULONG_MAX
 
 unsigned long intervalStart = 0;
@@ -40,29 +47,32 @@ void setup() {
 
   pinMode(TIMER_PIN, INPUT_PULLUP);
   attachInterrupt(digitalPinToInterrupt(TIMER_PIN), relayOn, FALLING);
+
+  changeTimerState();
 }
 
 void loop() {
 
   unsigned int intervalEnd_prev;
   unsigned int intervalStart_prev;
-
-  //counter.fillSprite(TFT_BLACK);
+  
+  counter.fillSprite(TFT_BLACK);
 
   //if ((intervalEnd - intervalStart) < COUNTER_DELAY) timerState = true;
   //if ((intervalStart - intervalEnd) < COUNTER_DELAY) timerState = false;
 
   switch (timerState) {
-    case 0:
+    case TIMER_RUN:
       counter.setTextColor(TFT_WHITE);
       counter.drawFloat((float)(micros() - intervalStart) / 1000000, 2, SCREEN_W / 2, SCREEN_H / 2);
       break;
-    case 1:
+    case TIMER_STOP:
       counter.setTextColor(TFT_GREEN);
       counter.drawFloat((float)(intervalEnd - intervalStart) / 1000000, 2, SCREEN_W / 2, SCREEN_H / 2);
       break;
-    case 2:
-      // statements
+    case TIMER_RESET:
+      counter.setTextColor(TFT_BLUE);
+      counter.drawFloat(0.0, 2, SCREEN_W / 2, SCREEN_H / 2);
       break;
   }
   /*
@@ -82,10 +92,10 @@ void loop() {
 void relayOn() {
 
   timePoint = micros();
-  if (((timePoint - timePointPrev) > COUNTER_DELAY) || timePoint == 0) {
+  if (((timePoint - timePointPrev) > COUNTER_DELAY) || timePoint == 0 || timerState == 2) {
     timerState = changeTimerState();
-    if (timerState) intervalStart = timePoint;
-    if (!timerState) intervalEnd = timePoint;
+    if (timerState == 0) intervalStart = timePoint;
+    if (timerState == 1) intervalEnd = timePoint;
     timePointPrev = timePoint;
   }
 }
